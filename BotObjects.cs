2eb3a812c0c85200f5695ad3a58a6b63fee0eed8
JettryX <alexfrei21@gmail.com>
2024-08-08 15:12:50 +0300

    Remove useless libs

--- BotObjects.cs
+++ BotObjects.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
