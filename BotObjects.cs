e0dc62dcda1857c8edbdfd5db54485a84d196357
JettryX <alexfrei21@gmail.com>
2024-08-08 15:11:05 +0300

    Add project files.

---
+++ BotObjects.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+class BasicCommand
+{
+    protected String _command;
+    protected String _description;
+    public String command
+    {
+        get
+        {
+            return _command;
+        }
+        private set { }
+    }
+    public String description
+    {
+        get
+        {
+            return _description;
+        }
+        private set { }
+    }
+}
+class BotCommand<X> : BasicCommand
+{
+    public delegate String CommandHandler(X arg);
+    public CommandHandler handler;
+
+    public BotCommand(string command, string description, CommandHandler handler)
+    {
+        _command = command;
+        _description = description;
+        this.handler = handler;
+    }
+
+    public String Handle(X arg)
+    {
+        return handler.Invoke(arg);
+    }
+}
+
+class INN_Client
+{
+    private String _token;
+    private String _url;
+    private HttpClient client;
+
+    public INN_Client(string token, string url)
+    {
+        _token = token;
+        _url = url;
+    }
+
+    public void InitClient() 
+    {
+        client = new HttpClient();
+        client.BaseAddress = new Uri(_url);
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<String> GetResponse(String paramName, String value)
+    {
+        String res = "";
+        String query = $"company?key={_token}&{paramName}={value}";
+        HttpResponseMessage response = await client.GetAsync(client.BaseAddress + query);
+        if (response.IsSuccessStatusCode)
+        {
+            res = await response.Content.ReadAsStringAsync();
+            Company comp = JsonSerializer.Deserialize<Company>(res);
+            if (comp.meta.message == null)
+            {
+                res = comp.getInfo();
+            } else
+            {
+                res = "Организация не найдена";
+            }
+
+        }
+        return res;
+    }
+}
+
+public class Company
+{
+    public CompanyMainData data { get; set; }
+    public Meta meta { get; set; }
+
+    public String getInfo()
+    {
+        return $"НАИМЕНОВАНИЕ: {data?.Name}\nАДРЕС: {data?.LegalAddress?.Address}";
+    }
+}
+
+public class CompanyMainData
+{
+    [JsonPropertyName("НаимПолн")]
+    public String Name { get; set; }
+
+    [JsonPropertyName("ЮрАдрес")]
+    public CompanyLegalAddress LegalAddress { get; set; }
+}
+
+public class CompanyLegalAddress
+{
+    [JsonPropertyName("АдресРФ")]
+    public String Address { get; set; }
+}
+
+public class Meta
+{
+    public String status { get; set; }
+    public String message { get; set; }
+}
+
+public class Configuration
+{
+    private IConfigurationRoot IConfigRoot;
+
+    public Configuration(String filePath)
+    {
+        IConfigRoot = new ConfigurationBuilder().AddJsonFile(filePath, false, false).Build();
+    }
+
+    public String getConfigValue(string key)
+    {
+        String? value = "";
+        try
+        {
+            value = IConfigRoot.GetValue<String>(key);
+        } 
+        catch (Exception ex) { }
+
+        return value;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TGBot_Main
+{
+    class Program
+    {
+        static String AUTHOR_MSG = "Alex Odinokov (Frei), alexfrei21@gmail.com, https://github.com/JettryX";
+
+        static Dictionary<string, BasicCommand> botCommands = new Dictionary<string, BasicCommand>();
+        static Dictionary<long, string> lastCommandsPerUser = new Dictionary<long, string>();
+        static INN_Client innClient;
+        static TelegramBotClient telegramBot;
+        static Configuration _configuration = new Configuration("app.Settings.json"); // подгрузка конфигурации
+
+        static void Main(string[] args)
+        {
+            // Инициализация основных функций бота
+            try
+            {
+                InitCommands();
+                innClient = new INN_Client(_configuration.getConfigValue("INN_Token"), "https://api.ofdata.ru/v2/");
+                innClient.InitClient();
+
+                using var cts = new CancellationTokenSource();
+                string TG_token = _configuration.getConfigValue("TG_Token");
+                TelegramBotClientOptions tgClientOptions = new TelegramBotClientOptions(TG_token);
+                telegramBot = new TelegramBotClient(tgClientOptions, cancellationToken: cts.Token);
+                telegramBot.OnMessage += OnMessageHandler;
+
+                Console.WriteLine("Bot is running");
+                Console.ReadLine();
+                cts.Cancel();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+        }
+
+        // обработка текстовых команд
+        static async Task OnMessageHandler(Message msg, UpdateType type)
+        {
+            if (msg.Text == null) return;
+
+            Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+
+            if (msg.Text.StartsWith('/'))
+            {
+                switch (type)
+                {
+                    case UpdateType.Message:
+                        String response = getTextResponse(msg);
+                        await telegramBot.SendTextMessageAsync(msg.Chat, response);
+                        lastCommandsPerUser[msg.Chat.Id] = response;
+                        break;
+                    // можно захендлить другие типы дальше    
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Инициализация команд и биндинг их к обработчикам
+        static void InitCommands()
+        {
+            BasicCommand startCommand = new BotCommand<Message>("/start", "Начало работы с ботом", (Message msg) => { return _configuration.getConfigValue("GREET_MSG");});
+            botCommands.Add(startCommand.command, startCommand);
+
+            BasicCommand helpCommand = new BotCommand<Message>("/help", "Показывает все доступные команды", HandleHelp);
+            botCommands.Add(helpCommand.command, helpCommand);
+
+            BasicCommand helloCommand = new BotCommand<Message>("/hello", "Показывает автора, его email и ссылку на github", (Message msg) => { return AUTHOR_MSG;});
+            botCommands.Add(helloCommand.command, helloCommand);
+
+            BasicCommand innCommand = new BotCommand<Message>("/inn", "Поиск информации о компании по ИНН на сайте ofdata.ru", HandleInnSearch);
+            botCommands.Add(innCommand.command, innCommand);
+
+            BasicCommand lastCommand = new BotCommand<Message>("/last", "Повторяет последнее выполненое действие", HandleLast);
+            botCommands.Add(lastCommand.command, lastCommand);
+        }
+
+        static String getTextResponse(Message msg)
+        {
+            String response = "";
+            try
+            {
+                String[] strParams = msg.Text.Split(" ");
+                if (botCommands.ContainsKey(strParams[0]))
+                {
+                    BasicCommand x = botCommands[strParams[0]];
+                    response = ((BotCommand<Message>)x).Handle(msg);
+                }
+             
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return response;
+        }
+
+        static String HandleHelp(Message msg)
+        {
+            String strHelp = "";
+            foreach (String s in botCommands.Keys)
+            {
+                strHelp += botCommands[s].command + " - " + botCommands[s].description + '\n';
+            }
+            return strHelp;
+        }
+
+        static String HandleLast(Message msg)
+        {
+            return lastCommandsPerUser.ContainsKey(msg.Chat.Id) ? lastCommandsPerUser[msg.Chat.Id] : "Предыдущие команды не найдены";
+        }
+
+        static String HandleInnSearch(Message msg)
+        {
+            String[] msgParams = msg.Text.Split(" ");
+            if (msgParams.Length > 1)
+            {
+                String res = innClient.GetResponse("inn", msgParams[1]).GetAwaiter().GetResult();
+                return res;
+            }
+            return "Пожалуйста укажите искомый ИНН /inn <искомый инн>";
+        }
+
+    }
+
+}
